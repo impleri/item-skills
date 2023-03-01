e2be10c7fd3a10e85ba8640d8b6bef6f0a84358f
Christopher Roussel <christopher@impleri.net>
2023-02-28 21:26:28 -0600

    feature: Trinkets integration in Fabric

---
+++ fabric/src/main/java/net/impleri/itemskills/integrations/trinkets/TrinketsSkills.java
@@ -0,0 +1,35 @@
+package net.impleri.itemskills.integrations.trinkets;
+
+import dev.emi.trinkets.api.TrinketComponent;
+import dev.emi.trinkets.api.TrinketsApi;
+import net.impleri.itemskills.InventoryHelper;
+import net.impleri.itemskills.ItemHelper;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.function.Consumer;
+
+public class TrinketsSkills {
+    public static void onServerTick(MinecraftServer server) {
+        server.getPlayerList().getPlayers().forEach(TrinketsSkills::onPlayerTick);
+
+    }
+
+    private static void onPlayerTick(Player player) {
+        TrinketsApi.getTrinketComponent(player).ifPresent(handleTrinket(player));
+    }
+
+    private static Consumer<TrinketComponent> handleTrinket(Player player) {
+        return (component) -> {
+            component.getAllEquipped().forEach(tuple -> {
+                var stack = tuple.getB();
+                if (!ItemHelper.isWearable(player, stack.getItem())) {
+                    var slot = tuple.getA();
+                    slot.inventory().setItem(slot.index(), ItemStack.EMPTY);
+                    InventoryHelper.moveItemIntoInventory(player, stack);
+                }
+            });
+        };
+    }
+}
