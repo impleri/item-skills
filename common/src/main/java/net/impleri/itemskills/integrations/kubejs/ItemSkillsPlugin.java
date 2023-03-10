dbd98a170cb8ef67f5e71b51ea3b8933c7b5b57e
Christopher Roussel <christopher@impleri.net>
2023-03-10 12:31:53 -0600

    Performance improvements (#25)
    * chore: swap out semver plugin
    
    * fix: Improve performance via PlayerSkills update and caching
    
    * chore: Update to unified publishing
    
    * chore: Make kubejs optional because of technicalities

--- common/src/main/java/net/impleri/itemskills/integrations/kubejs/ItemSkillsPlugin.java
+++ common/src/main/java/net/impleri/itemskills/integrations/kubejs/ItemSkillsPlugin.java
@@ -3,7 +3,6 @@ package net.impleri.itemskills.integrations.kubejs;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import net.impleri.itemskills.integrations.kubejs.events.EventsBinding;
 import net.impleri.itemskills.integrations.kubejs.events.RestrictionsRegistrationEventJS;
-import net.impleri.itemskills.restrictions.Registry;
 
 public class ItemSkillsPlugin extends KubeJSPlugin {
     @Override
@@ -12,7 +11,6 @@ public class ItemSkillsPlugin extends KubeJSPlugin {
     }
 
     public static void onStartup() {
-        Registry.INSTANCE.clear();
         EventsBinding.RESTRICTIONS.post(new RestrictionsRegistrationEventJS());
     }
 }
--- common/src/main/java/net/impleri/itemskills/integrations/rei/SkillsDisplayVisibility.java
+++ common/src/main/java/net/impleri/itemskills/integrations/rei/SkillsDisplayVisibility.java
@@ -7,13 +7,28 @@ import me.shedaniel.rei.api.common.display.Display;
 import me.shedaniel.rei.api.common.entry.EntryIngredient;
 import me.shedaniel.rei.api.common.entry.EntryStack;
 import net.impleri.itemskills.client.ClientApi;
+import net.impleri.playerskills.client.events.ClientSkillsUpdatedEvent;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
 public class SkillsDisplayVisibility implements DisplayVisibilityPredicate {
+    private final Map<Item, Boolean> hiddenProducibles = new HashMap<>();
+    private final Map<Item, Boolean> hiddenConsumables = new HashMap<>();
+
+    public SkillsDisplayVisibility() {
+        ClientSkillsUpdatedEvent.EVENT.register(this::clearCache);
+    }
+
+    private void clearCache(ClientSkillsUpdatedEvent clientSkillsUpdatedEvent) {
+        hiddenProducibles.clear();
+        hiddenConsumables.clear();
+    }
+
     @Override
     public double getPriority() {
         return 100.0;
@@ -42,32 +57,48 @@ public class SkillsDisplayVisibility implements DisplayVisibilityPredicate {
     /**
      * Checks every ingredient to see if any are uncraftable
      */
+    private boolean isntProducible(Item item) {
+        return !hiddenProducibles.computeIfAbsent(item, ClientApi.INSTANCE::isProducible);
+    }
+
     private boolean hasHiddenOutput(EntryStack<?> entry) {
+        if (entry.isEmpty()) {
+            return false;
+        }
+
         var value = entry.getValue();
 
         if (value instanceof Item item) {
-            return !ClientApi.INSTANCE.isProducible(item);
+            return isntProducible(item);
         }
 
         if (value instanceof ItemStack stack) {
-            return !ClientApi.INSTANCE.isProducible(stack.getItem());
+            return !((ItemStack) value).isEmpty() && isntProducible(stack.getItem());
         }
 
         return false;
+    }
+
+    private boolean isntConsumable(Item item) {
+        return !hiddenConsumables.computeIfAbsent(item, ClientApi.INSTANCE::isConsumable);
     }
 
     /**
      * Checks every ingredient to see if any are supposed to be hidden
      */
     private boolean hasHiddenInput(EntryStack<?> entry) {
+        if (entry.isEmpty()) {
+            return false;
+        }
+
         var value = entry.getValue();
 
         if (value instanceof Item item) {
-            return !ClientApi.INSTANCE.isConsumable(item);
+            return isntConsumable(item);
         }
 
         if (value instanceof ItemStack stack) {
-            return !ClientApi.INSTANCE.isConsumable(stack.getItem());
+            return !((ItemStack) value).isEmpty() && isntConsumable(stack.getItem());
         }
 
         return false;
