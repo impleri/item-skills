e9f27cf3e131eb4bbe994e72265f8986e154f00f
Christopher Roussel <christopher@impleri.net>
2023-03-21 22:17:57 -0500

    fix: Get curios integration working again

--- forge/src/main/java/net/impleri/itemskills/forge/ItemSkillsForge.java
+++ forge/src/main/java/net/impleri/itemskills/forge/ItemSkillsForge.java
@@ -2,11 +2,6 @@ package net.impleri.itemskills.forge;
 
 import dev.architectury.platform.forge.EventBuses;
 import net.impleri.itemskills.ItemSkills;
-import net.impleri.itemskills.integrations.curios.CuriosSkills;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.LogicalSide;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -16,12 +11,5 @@ public class ItemSkillsForge {
         // Submit our event bus to let architectury register our content on the right time
         EventBuses.registerModEventBus(ItemSkills.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         ItemSkills.init();
-    }
-
-    @SubscribeEvent
-    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
-        if (event.side == LogicalSide.SERVER && ModList.get().isLoaded("curios")) {
-            CuriosSkills.onPlayerTick(event.player);
-        }
     }
 }
