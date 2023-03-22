1dcab40e56b3c71e81ed32a7e80acab2b611de46
Christopher Roussel <christopher@impleri.net>
2023-03-21 22:13:43 -0500

    feature: Implement Skills 1.7.0 with dimension and biome rules

--- common/src/main/java/net/impleri/itemskills/integrations/kubejs/ItemSkillsPlugin.java
+++ common/src/main/java/net/impleri/itemskills/integrations/kubejs/ItemSkillsPlugin.java
@@ -3,6 +3,7 @@ package net.impleri.itemskills.integrations.kubejs;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import net.impleri.itemskills.integrations.kubejs.events.EventsBinding;
 import net.impleri.itemskills.integrations.kubejs.events.RestrictionsRegistrationEventJS;
+import net.minecraft.server.MinecraftServer;
 
 public class ItemSkillsPlugin extends KubeJSPlugin {
     @Override
@@ -10,7 +11,7 @@ public class ItemSkillsPlugin extends KubeJSPlugin {
         EventsBinding.GROUP.register();
     }
 
-    public static void onStartup() {
-        EventsBinding.RESTRICTIONS.post(new RestrictionsRegistrationEventJS());
+    public static void onStartup(MinecraftServer server) {
+        EventsBinding.RESTRICTIONS.post(new RestrictionsRegistrationEventJS(server));
     }
 }
